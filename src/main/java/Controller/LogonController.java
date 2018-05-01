f1584229fcb0ce48e8eedcec6b73d4a65e434e78
eligoodwin <goodwiel@oregonstate.edu>
2018-05-01 15:59:16 -0700

    added some simple pages

--- src/main/java/Controller/LogonController.java
+++ src/main/java/Controller/LogonController.java
@@ -4,5 +4,6 @@ import javafx.event.ActionEvent;
 
 public class LogonController {
     public void gotoNextPage(ActionEvent actionEvent) {
+        System.out.println("Logon controller clicked");
     }
 }
